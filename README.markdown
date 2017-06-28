9e73ee5d45c37a96c68b0ee7a1e3b9a0f256fac7
林博仁 <Buo.Ren.Lin@gmail.com>
2017-06-28 21:51:11 +0800

    開始版本控制
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

---
+++ README.markdown
@@ -0,0 +1,22 @@
+# .bashrc.d
+This is a distributed bash run commands(rc) implementation
+
+<https://github.com/Lin-Buo-Ren/.bashrc.d>
+
+## How to use
+Run the following command in your home directory in terminal:
+
+```sh
+git clone https://github.com/Lin-Buo-Ren/.bashrc.d
+echo -e '\n# Setup .bashrc.d\n# https://github.com/Lin-Buo-Ren/.bashrc.d\nsource "${HOME}/.bashrc.d/README.setup"\n' >> "${HOME}/.bashrc"
+
+```
+
+Then restart Bash to make it affect
+
+## Author
+林博仁 &lt;<Buo.Ren.Lin@gmail.com>&gt;
+
+## License
+GNU GPLv3+
+
---
+++ Setup Powerline Shell.source.bash
@@ -0,0 +1,35 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034
+# Setup Powerline Shell
+# 林博仁 © 2017
+
+## Include Guard to prevent multiple sourcing
+if [ -n "${BASHRC_D_SETUP_POWERLINE_SHELL}" ]; then
+	return 0
+fi
+
+declare powerline_shell_location="$HOME/軟體/Powerline Shell: A Powerline style prompt for your shell"
+
+if [ ! -f "$HOME/軟體/Powerline Shell: A Powerline style prompt for your shell/powerline-shell.py" ]; then
+	printf --\
+		"%s: Warning: Powerline installation not detected, skipping setup.\n"\
+		"${BASH_SOURCE}"
+	return 0
+fi
+
+function _update_ps1() {
+    PS1="$("${powerline_shell_location}"/powerline-shell.py $? 2> /dev/null)"
+}
+
+if [ "$TERM" != "linux" ]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
+
+## Set Include Guard
+declare BASHRC_D_SETUP_POWERLINE_SHELL=1
+
+## This script is based on the GNU Bash Shell Script Template project
+## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
+## and is based on the following version:
+declare META_BASED_ON_GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v1.24.2-3-g3ec093a-dirty"
+## You may rebase your script to incorporate new features and fixes from the template
