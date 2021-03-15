585c89cb7e1d9975e15b08b0ab1bc9556cf405af
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2021-03-16 04:31:49 +0800

    powerline: Use Debian installation of powerline
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- Setup Powerline Shell.source.bash
+++ Setup Powerline Shell.source.bash
@@ -8,22 +8,8 @@ if [ -n "${BASHRC_D_SETUP_POWERLINE_SHELL}" ]; then
 	return 0
 fi
 
-declare powerline_shell_installation="$HOME/軟體/Powerline Shell: A Powerline style prompt for your shell"
-
-if [ ! -f "${powerline_shell_installation}/powerline-shell.py" ]; then
-	printf --\
-		"%s: Warning: Powerline installation not detected, skipping setup.\n"\
-		"${BASH_SOURCE[0]}"\
-		1>&2
-	return 0
-fi
-
-function _update_ps1() {
-	PS1="$("${powerline_shell_location}"/powerline-shell.py $? 2> /dev/null)"
-}
-
-if [ "$TERM" != "linux" ]; then
-	PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+if test -e /usr/share/powerline/bindings/bash/powerline.sh; then
+	. /usr/share/powerline/bindings/bash/powerline.sh
 fi
 
 ## Set Include Guard
